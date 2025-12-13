212dc97061d609effb5222cafa3d95928730a214
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-13 17:50:00 +0200

    Delete (routes/routes/routes/paypalRoutes.js

--- (routes/routes/routes/paypalRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const paypalController = require("../controllers/paypalController");
-
-router.post("/create-order", paypalController.createOrder);
-router.post("/capture-order", paypalController.captureOrder);
-
-module.exports = router;
