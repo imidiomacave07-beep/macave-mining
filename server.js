26a25274bae1a47edd16602bbba239a8587abe02
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-13 15:04:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
@@ -12,15 +13,20 @@ app.use(express.json());
 // Servir arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, "public")));
 
-// Rotas
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
-
-// Rota padrão
+// Rota raiz abre diretamente test-register.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/test-register.html"));
 });
 
+// Rotas da API de autenticação
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+// Outras rotas da plataforma (caso tenha)
+const miningRoutes = require("./routes/miningRoutes");
+app.use("/api/mining", miningRoutes);
+
+// Escutar a porta
 app.listen(PORT, () => {
   console.log(`Servidor ativo na porta ${PORT}`);
 });
