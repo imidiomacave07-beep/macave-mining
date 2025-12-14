61edbadeabe5b72ef631cf5fd27e53d81b198070
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-14 06:54:56 +0200

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,7 +1,1 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB conectado com sucesso"))
-  .catch((err) => console.error("Erro ao conectar MongoDB:", err));
