4d6b5d6ec51558d755b03cc338f52560d742d784
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-14 14:04:10 +0200

    Update authRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs'); // ✅ usar bcryptjs
 const User = require('../User'); // modelo de usuário
 
 // Registro de usuário
