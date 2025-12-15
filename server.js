57e3c2ff9d51f073627938772804f2a5bee1d79f
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-15 07:34:54 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,33 +1,21 @@
-// server.js
-const express = require("express");
-const path = require("path");
-require("dotenv").config();
-require("./connect");
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+require('./connect');
 
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Arquivos estáticos
-app.use(express.static(path.join(__dirname, "public")));
-
-// Página inicial
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/login.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/test-register.html'));
 });
 
-// Rotas
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
-// 🔴 TEMPORARIAMENTE COMENTADO se não existir
-// const miningRoutes = require("./routes/miningRoutes");
-// app.use("/api/mining", miningRoutes);
-
-// Servidor
 app.listen(PORT, () => {
   console.log(`Servidor ativo na porta ${PORT}`);
 });
