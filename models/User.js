8d4043412d9bb66fe78277b1fcdba5b08f074441
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-15 07:11:50 +0200

    Create User.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user"
+  },
+
+  country: {
+    type: String,
+    default: "Unknown"
+  },
+
+  balance: {
+    type: Number,
+    default: 0
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
