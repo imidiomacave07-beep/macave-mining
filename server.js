e2164b64e9c33d8b927249cf7f7132de84f41a1d
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-16 18:03:00 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const path = require("path");
-require("dotenv").config();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); // SERVE OS ARQUIVOS HTML
 
-require("./connect");
+// Conectar ao MongoDB
+const connectDB = require("./connect");
+connectDB();
 
+// Rotas
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
 
+// Rodar servidor
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log("Servidor rodando na porta", PORT));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
