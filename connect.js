862ecc6e8d4eb6b7ff6a87621f20893c320a9741
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-16 18:04:07 +0200

    Update connect.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- connect.js
+++ connect.js
@@ -1,8 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB conectado!'))
-.catch(err => console.error('Erro ao conectar MongoDB:', err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB conectado!");
+  } catch (err) {
+    console.error("Erro ao conectar MongoDB:", err);
+  }
+};
+
+module.exports = connectDB;
