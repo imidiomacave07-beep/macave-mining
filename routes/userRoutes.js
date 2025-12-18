d49837bcc8984a98893032df6d20472c9ef7365b
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-18 16:12:03 +0200

    Update userRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
+// routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
+const { register, login } = require('../controllers/userController');
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+// Rotas
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
