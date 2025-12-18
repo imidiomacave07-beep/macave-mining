1d70fd735a06d021f707b72cc71695f22f8ab64d
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-18 22:08:47 +0200

    Update authRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,33 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+
+const {
+  registerUser,
+  loginUser
+} = require('../controllers/userController');
 
 // Registro
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) return res.status(400).json({ message: "Preencha todos os campos" });
-
-  const existingUser = await User.findOne({ username });
-  if (existingUser) return res.status(400).json({ message: "Usuário já existe" });
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({ username, password: hashedPassword });
-  await newUser.save();
-
-  res.status(201).json({ message: "Usuário registrado com sucesso!" });
-});
+router.post('/register', registerUser);
 
 // Login
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  if (!user) return res.status(400).json({ message: "Usuário não encontrado" });
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(400).json({ message: "Senha incorreta" });
-
-  res.status(200).json({ message: "Login bem-sucedido", user: { username: user.username } });
-});
+router.post('/login', loginUser);
 
 module.exports = router;
