a30c1e10d6db3ac5939eef8d938789fd7a932a22
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-18 23:14:49 +0200

    Update userController.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,62 +2,27 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-// REGISTER
-exports.registerUser = async (req, res) => {
+exports.register = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
-
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ message: 'Usuário já existe' });
-    }
-
+    const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword
-    });
-
-    res.status(201).json({
-      message: 'Usuário registrado com sucesso',
-      user
-    });
-
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+    const user = await User.create({ username, password: hashedPassword });
+    res.status(201).json({ message: 'Usuário registrado com sucesso' });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
 };
 
-// LOGIN
-exports.loginUser = async (req, res) => {
+exports.login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: 'Usuário não encontrado' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ message: 'Senha incorreta' });
-    }
-
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET || 'macave_secret',
-      { expiresIn: '1d' }
-    );
-
-    res.json({
-      message: 'Login efetuado com sucesso',
-      token,
-      user
-    });
-
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(401).json({ message: 'Senha incorreta' });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Login realizado com sucesso', token, balance: user.balance });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
