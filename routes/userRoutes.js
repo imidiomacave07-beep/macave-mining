3ee4765ec60e82b154afaea0f2c8aa05ff2eb1e3
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-20 04:17:45 +0200

    Update userRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,26 @@
+require('dotenv').config();
 const express = require('express');
-const router = express.Router();
-const {
-  registerUser,
-  loginUser
-} = require('../controllers/userController');
+const cors = require('cors');
+const connectDB = require('./config/connect');
 
-// ROTAS PÚBLICAS
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+const app = express();
 
-module.exports = router;
+// Conectar MongoDB
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Rota teste
+app.get('/', (req, res) => {
+  res.send('Macave Mining API está rodando');
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
