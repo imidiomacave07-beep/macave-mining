889dd742eaf7f5907603d5dd7456f8876c5035e8
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-20 04:10:09 +0200

    Update userRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,12 @@
 const express = require('express');
-const { register, login } = require('../controllers/userController');
 const router = express.Router();
+const {
+  registerUser,
+  loginUser
+} = require('../controllers/userController');
 
-// Registrar usuário
-router.post('/register', register);
-
-// Login do usuário
-router.post('/login', login);
-
-// Mineração simulada
-router.post('/mine', async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id); // autenticação JWT necessária
-    const earned = Math.floor(Math.random() * 10); // ganho simulado
-    user.balance += earned;
-    await user.save();
-    res.json({ message: 'Mineração concluída', earned, balance: user.balance });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// ROTAS PÚBLICAS
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
