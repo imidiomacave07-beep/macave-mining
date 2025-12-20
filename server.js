bf352a7742f648883c82718a81bf30e96c639e29
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-20 04:44:37 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 const connectDB = require('./config/connect');
 
 const app = express();
 
-// Conectar MongoDB
-connectDB();
-
-// Middlewares
+// middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rotas
-app.use('/api/users', require('./routes/userRoutes'));
+// conexão DB
+connectDB();
 
-// Rota teste
+// rota teste
 app.get('/', (req, res) => {
   res.send('Macave Mining API está rodando');
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`Servidor rodando na porta ${PORT}`)
+);
