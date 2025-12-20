6f2950af37bb1bc7f3bd03c39ba776a700f02f01
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-20 15:43:46 +0200

    Update userController.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,38 +2,34 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-exports.register = async (req, res) => {
+const registerUser = async (req, res) => {
   const { username, password } = req.body;
   try {
-    let user = await User.findOne({ username });
-    if (user) return res.status(400).json({ message: 'Usuário já existe' });
-
     const hashedPassword = await bcrypt.hash(password, 10);
-    user = await User.create({ username, password: hashedPassword, balance: 0 });
-
-    res.json({ message: 'Usuário registrado com sucesso' });
+    const newUser = await User.create({ username, password: hashedPassword, balance: 0 });
+    res.status(201).json({ message: 'Usuário registrado', user: newUser });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-exports.login = async (req, res) => {
+const loginUser = async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await User.findOne({ username });
-    if (!user) return res.status(400).json({ message: 'Usuário não encontrado' });
+    if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Senha incorreta' });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-    res.json({ token, username: user.username, balance: user.balance });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Login bem-sucedido', token });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-exports.mine = async (req, res) => {
+const mine = async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
     const earned = Math.floor(Math.random() * 10);
@@ -44,3 +40,5 @@ exports.mine = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+module.exports = { registerUser, loginUser, mine };
