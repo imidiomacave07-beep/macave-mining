514fb7581aa7d15b8e06bb549f13174dacc9c892
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-20 15:41:54 +0200

    Update userRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const { registerUser, loginUser, mine } = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-router.post('/mine', authMiddleware, userController.mine);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/mine', authMiddleware, mine);
 
 module.exports = router;
