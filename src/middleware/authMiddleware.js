55efd31f6a327561c358503a3944654af531ef18
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-24 13:17:27 +0200

    Update authMiddleware.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
-const authMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'Token não fornecido' });
+module.exports = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não fornecido' });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
+    req.user = decoded;
     next();
-  } catch (err) {
+  } catch (error) {
     res.status(401).json({ message: 'Token inválido' });
   }
 };
-
-module.exports = authMiddleware;
