9b2c245685580bdcb378d966f5274bfe91e76ccd
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-28 08:08:20 +0200

    Update authRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,75 +1,8 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
 const router = express.Router();
+const { register, login } = require('../controllers/userController');
 
-/* REGISTRO */
-router.post('/register', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    if (!name || !email || !password) {
-      return res.status(400).json({ msg: 'Preencha todos os campos' });
-    }
-
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ msg: 'Usuário já existe' });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword
-    });
-
-    res.status(201).json({ msg: 'Conta criada com sucesso' });
-
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-/* LOGIN */
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ msg: 'Credenciais inválidas' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ msg: 'Credenciais inválidas' });
-    }
-
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET || 'macave_secret',
-      { expiresIn: '1d' }
-    );
-
-    res.json({
-      msg: 'Login feito com sucesso',
-      token,
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        balance: user.balance
-      }
-    });
-
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
