0b5f2ab9134d665dbbcfb3a0df5d3d620656a62e
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-31 00:47:16 +0200

    Update authController.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,13 @@
-const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Preencha todos os campos" });
+    }
 
     const userExists = await User.findOne({ email });
     if (userExists) {
@@ -16,37 +19,19 @@ exports.register = async (req, res) => {
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password: hashedPassword
     });
 
-    res.status(201).json({ message: "Usuário registrado com sucesso" });
+    res.status(201).json({
+      message: "Usuário registrado com sucesso",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
   } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "Credenciais inválidas" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Credenciais inválidas" });
-    }
-
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1d" }
-    );
-
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ message: "Erro no servidor" });
   }
 };
