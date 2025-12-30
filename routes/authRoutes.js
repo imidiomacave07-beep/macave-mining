3f9dc9b17fbfd2ba305aaccb743bf52638d1ded9
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-30 13:15:52 +0200

    Update authRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { register, login } = require('../controllers/userController');
+const { register } = require("../controllers/authController");
 
-router.post('/register', register);
-router.post('/login', login);
+router.post("/register", register);
 
 module.exports = router;
