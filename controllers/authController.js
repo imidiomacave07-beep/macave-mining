4bf76c5163bd1498832ae9bccef630c0b21f58d8
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-30 13:50:57 +0200

    Update authController.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,43 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+/* =========================
+   REGISTER
+========================= */
+exports.register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Preencha todos os campos" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Usuário já existe" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+      balance: 0
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: "Usuário registrado com sucesso" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/* =========================
+   LOGIN
+========================= */
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
