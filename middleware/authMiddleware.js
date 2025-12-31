20b7f35a870f1e78fd8a308542af7c10806b00d7
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-12-31 17:50:51 +0200

    Update authMiddleware.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,7 @@
-const jwt = require("jsonwebtoken");
-
-module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).json({ error: "Token não fornecido" });
-
-  const token = authHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id;
-    next();
-  } catch (err) {
-    res.status(401).json({ error: "Token inválido" });
-  }
-};
+server.js'
+node:internal/modules/cjs/loader:1404
+  throw err;
+  ^
+Error: Cannot find module '../middlewares/authMiddleware'
+Require stack:
+- /opt/render/project/src/routes/authRoutes.js
