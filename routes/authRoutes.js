e043393ce1c72802c5a39e04594c07ef5cae0fc7
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-01 12:55:00 +0200

    Update authRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,25 +1,14 @@
 const express = require("express");
-const fs = require("fs");
-const authMiddleware = require("../middlewares/authMiddleware");
-const User = require("../models/User");
-
 const router = express.Router();
 
-// Listar planos
-router.get("/", (req, res) => {
-  const plans = JSON.parse(fs.readFileSync("./public/plans.json"));
-  res.json(plans);
-});
+const authMiddleware = require("../middlewares/authMiddleware");
 
-// Comprar plano
-router.post("/buy", authMiddleware, async (req, res) => {
-  const { planName, planPrice } = req.body;
-  const user = await User.findById(req.userId);
-  if (!user) return res.status(404).json({ error: "Usuário não encontrado" });
-  if (user.balance < planPrice) return res.status(400).json({ error: "Saldo insuficiente" });
-  user.balance -= planPrice;
-  await user.save();
-  res.json({ message: `Plano ${planName} comprado com sucesso!`, balance: user.balance });
+router.get("/me", authMiddleware, async (req, res) => {
+  res.json({
+    name: "Imídio",
+    email: "imidiomacave07@gmail.com",
+    balance: 0
+  });
 });
 
 module.exports = router;
