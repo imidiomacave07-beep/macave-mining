4b36fc8c26e66da7962e6681750155a9cb48ba0f
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-02 13:51:32 +0200

    Update authRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-// ✅ caminho CORRETO (middleware, singular)
 const authMiddleware = require("../middleware/authMiddleware");
 
-// rota protegida
 router.get("/me", authMiddleware, (req, res) => {
   res.json({
     id: req.userId,
     name: "Usuário Macave",
-    email: "imidiomacave07@gmail.com",
+    email: "teste@macave.com",
     balance: 0
   });
 });
