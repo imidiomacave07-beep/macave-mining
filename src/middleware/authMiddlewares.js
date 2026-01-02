6025d25de7e3958dcf80572b55d297fa45df3240
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-02 07:25:00 +0200

    Update authMiddlewares.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/middleware/authMiddlewares.js
+++ src/middleware/authMiddlewares.js
@@ -1,6 +1,4 @@
-const jwt = require("jsonwebtoken");
-
-module.exports = (req, res, next) => {
+module.exports = function (req, res, next) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
@@ -10,9 +8,9 @@ module.exports = (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(
+    const decoded = require("jsonwebtoken").verify(
       token,
-      process.env.JWT_SECRET || "segredo123"
+      process.env.JWT_SECRET
     );
     req.user = decoded;
     next();
