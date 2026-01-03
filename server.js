1e6ac00d8546512f3b157c84932e8893639c3ac8
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-03 12:37:35 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+// 🔴 SEM ISTO O LOGIN NUNCA FUNCIONA
 app.use(express.json());
-
-// servir arquivos públicos
-app.use(express.static(path.join(__dirname, "public")));
 
 // rotas
 const authRoutes = require("./src/routes/authRoutes");
 app.use("/api/auth", authRoutes);
 
-// dashboard protegido
+// ficheiros públicos
+app.use(express.static(path.join(__dirname, "public")));
+
+// dashboard
 app.get("/dashboard", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "dashboard.html"));
 });
 
-// porta
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("🚀 Servidor rodando na porta", PORT);
+  console.log("Servidor rodando na porta", PORT);
 });
