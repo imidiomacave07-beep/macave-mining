6f972013a7e7c3a4ba1eb9adcf13d7551693ff6d
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-03 17:53:23 +0200

    Delete src directory
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/models/UserPlan.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserPlanSchema = new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  planId: mongoose.Schema.Types.ObjectId,
-  lastClaim: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("UserPlan", UserPlanSchema);
--- src/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const User = require("../models/User");
-const Plan = require("../models/Plan");
-const UserPlan = require("../models/UserPlan");
-const auth = require("../middleware/authMiddleware");
