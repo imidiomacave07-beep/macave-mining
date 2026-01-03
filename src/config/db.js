846d16c319c9dbd0d3e3e9ecea2de887f8225adf
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-03 17:25:18 +0200

    Update db.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-module.exports = async () => {
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB conectado");
+    console.log("MongoDB conectado");
   } catch (err) {
-    console.error("❌ Erro MongoDB", err);
+    console.error(err);
     process.exit(1);
   }
 };
+
+module.exports = connectDB;
