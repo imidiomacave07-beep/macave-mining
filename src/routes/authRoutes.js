2e8a17dc9e935e54236bec9212895d59ae41d436
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-03 15:04:00 +0200

    Update authRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,56 +1,17 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
-const User = require("../models/User");
-const authMiddleware = require("../middleware/authMiddleware");
-
 const router = express.Router();
 
-// REGISTAR
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+// caminho correto para middleware
+const authMiddleware = require("../middleware/authMiddleware");
 
-  const userExists = await User.findOne({ email });
-  if (userExists)
-    return res.status(400).json({ error: "Usuário já existe" });
-
-  const hash = await bcrypt.hash(password, 10);
-
-  const user = await User.create({
-    name,
-    email,
-    password: hash
+// rota de teste
+router.get("/me", authMiddleware, (req, res) => {
+  res.json({
+    id: req.userId,
+    name: "Usuário Macave",
+    email: "teste@macave.com",
+    balance: 0
   });
-
-  res.json({ message: "Conta criada com sucesso" });
-});
-
-// LOGIN
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-  if (!user)
-    return res.status(400).json({ error: "Usuário não encontrado" });
-
-  const valid = await bcrypt.compare(password, user.password);
-  if (!valid)
-    return res.status(401).json({ error: "Senha inválida" });
-
-  const token = jwt.sign(
-    { id: user._id },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
-
-  res.json({ token });
-});
-
-// USUÁRIO LOGADO
-router.get("/me", authMiddleware, async (req, res) => {
-  const user = await User.findById(req.userId).select("-password");
-  res.json(user);
 });
 
 module.exports = router;
