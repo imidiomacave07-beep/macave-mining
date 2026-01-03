0492475b77f7241a6a3d5f26be45c0fd3b52c16a
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-04 00:10:33 +0200

    Create authMiddleware.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token não fornecido" });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  jwt.verify(token, process.env.JWT_SECRET || "macave_secret", (err, decoded) => {
+    if (err) return res.status(401).json({ error: "Token inválido" });
+
+    req.userId = decoded.id;
+    next();
+  });
+};
