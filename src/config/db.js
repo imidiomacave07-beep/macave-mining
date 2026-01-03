fa9992a88d302c0b891d0c92d68d901b69604486
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-03 12:22:05 +0200

    Create db.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB conectado");
+  } catch (err) {
+    console.error("❌ Erro MongoDB", err);
+    process.exit(1);
+  }
+};
