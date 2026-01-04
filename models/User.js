ae5fc67ed0c28586c89830aa7b0ea51f8ff9e282
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-04 12:38:08 +0200

    Update User.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- models/User.js
+++ models/User.js
@@ -1,20 +1,16 @@
-const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
-module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res.status(401).json({ error: "Token não fornecido" });
+const UserSchema = new mongoose.Schema({
+  email: String,
+  plan: String,
+  balance: {
+    type: Number,
+    default: 0
+  },
+  lastEarning: {
+    type: Date,
+    default: Date.now
   }
+});
 
-  const [, token] = authHeader.split(" ");
-
-  jwt.verify(token, process.env.JWT_SECRET || "macave_secret", (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: "Token inválido" });
-    }
-
-    req.userId = decoded.id;
-    next();
-  });
-};
+module.exports = mongoose.model("User", UserSchema);
