c9188d1eb5d11d932f14a8683fd422cc7abf3939
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-04 13:28:42 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-app.use(express.json()); // para ler JSON do body
+app.use(express.json());
 
-// rotas da API
+// rotas
 app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/payment", require("./routes/paymentRoutes")); // <-- aqui
+app.use("/api/payment", require("./routes/paymentRoutes")); // <- caminho correto
 
-// rota raiz
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// iniciar servidor
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
