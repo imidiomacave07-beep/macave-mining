cb52738defd42649c3b8332be19ef8155f88a64c
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-05 23:29:17 +0200

    Update User.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- models/User.js
+++ models/User.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  plan: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   balance: {
     type: Number,
     default: 0
-  },
-  lastEarning: {
-    type: Date,
-    default: Date.now
   }
 });
 
