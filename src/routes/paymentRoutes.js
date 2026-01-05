a2ccb75977bd9a25a175a5e519452fbd64868ac1
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-05 20:32:44 +0200

    Update paymentRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,34 +1,7 @@
 const express = require("express");
-const authMiddleware = require("../middleware/authMiddleware");
-const Payment = require("../models/Payment");
-
 const router = express.Router();
+const { payWithCrypto } = require("../controllers/paymentController");
 
-// criar pedido de pagamento
-router.post("/payment", authMiddleware, async (req, res) => {
-  const { plan, amount, crypto } = req.body;
-
-  const payment = await Payment.create({
-    userId: req.userId,
-    plan,
-    amount,
-    crypto
-  });
-
-  res.json({
-    message: "Pedido criado",
-    wallet: getWallet(crypto),
-    payment
-  });
-});
-
-function getWallet(crypto) {
-  const wallets = {
-    USDT: "TXYZ-EXEMPLO-USDT-ENDERECO",
-    BTC: "bc1q-exemplo-btc",
-    ETH: "0xExemploEthWallet"
-  };
-  return wallets[crypto];
-}
+router.post("/crypto", payWithCrypto);
 
 module.exports = router;
