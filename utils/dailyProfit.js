b75bf03eccb921dda218bf12e6eebb2434d11d91
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 00:12:51 +0200

    Update dailyProfit.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- utils/dailyProfit.js
+++ utils/dailyProfit.js
@@ -1,3 +1,16 @@
-module.exports = function distributeDailyProfits() {
-  console.log("💰 Lucros diários distribuídos");
+const Plan = require("../models/Plan");
+const User = require("../models/User");
+
+module.exports = async function distributeDailyProfits() {
+  const plans = await Plan.find({ active: true });
+
+  for (const plan of plans) {
+    const profit = plan.amount * plan.dailyRate;
+
+    await User.findByIdAndUpdate(plan.userId, {
+      $inc: { balance: profit }
+    });
+  }
+
+  console.log("Lucros diários distribuídos");
 };
