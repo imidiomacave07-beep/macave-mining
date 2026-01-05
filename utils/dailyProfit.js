1c8596df2b6bbd58a1b5570601b87d780b649aa7
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-05 15:30:35 +0200

    Update dailyProfit.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- utils/dailyProfit.js
+++ utils/dailyProfit.js
@@ -1,31 +1,3 @@
-const Plan = require("../models/Plan");
-const User = require("../models/User");
-
-async function distributeDailyProfits() {
-  try {
-    const plans = await Plan.find({ active: true });
-
-    for (const plan of plans) {
-      const user = await User.findById(plan.userId);
-      if (!user) continue;
-
-      // calcula dias desde início
-      const daysElapsed = Math.floor((Date.now() - plan.startDate) / (1000 * 60 * 60 * 24));
-      if (daysElapsed >= plan.durationDays) {
-        plan.active = false;
-        await plan.save();
-        continue;
-      }
-
-      // adiciona lucro diário
-      user.balance += plan.dailyProfit;
-      await user.save();
-    }
-
-    console.log("Ganhos diários distribuídos com sucesso!");
-  } catch (err) {
-    console.error("Erro ao distribuir lucros diários:", err);
-  }
-}
-
-module.exports = distributeDailyProfits;
+module.exports = function distributeDailyProfits() {
+  console.log("💰 Lucros diários distribuídos");
+};
