9c2abcca6e66249acb7271bdf0f1308f7a6efce2
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-05 22:25:36 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,28 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
-const authRoutes = require("./src/routes/authRoutes");
-const dashboardRoutes = require("./src/routes/dashboardRoutes");
-const paymentRoutes = require("./src/routes/paymentRoutes");
-
+const path = require("path");
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
-// rotas
-app.use("/api/auth", authRoutes);
-app.use("/api/dashboard", dashboardRoutes);
-app.use("/api/payment", paymentRoutes);
+// SERVIR FRONTEND
+app.use(express.static(path.join(__dirname, "public")));
 
-// Conectar ao MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB conectado!"))
-.catch(err => console.log("Erro MongoDB:", err));
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+// rota principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
