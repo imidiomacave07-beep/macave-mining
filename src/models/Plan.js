31ce0d2016ab321ec0476be57e9ff644bad57a93
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-05 20:40:15 +0200

    Update Plan.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/models/Plan.js
+++ src/models/Plan.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
-const planSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },        // preço em USD
-  dailyProfit: { type: Number, required: true },  // lucro diário em %
-  durationDays: { type: Number, required: true }, // duração do plano
-  createdAt: { type: Date, default: Date.now }
+const PlanSchema = new mongoose.Schema({
+  name: String,
+  dailyProfit: Number
 });
 
-module.exports = mongoose.model("Plan", planSchema);
+module.exports = mongoose.model("Plan", PlanSchema);
