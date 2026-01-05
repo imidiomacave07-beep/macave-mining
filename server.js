f77c3a9b1cce9eeeea2cc9666587ebaff9da220c
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-05 14:27:41 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-
-const authRoutes = require("./routes/authRoutes");
-const paymentRoutes = require("./routes/paymentRoutes");
-
 const app = express();
-const PORT = process.env.PORT || 10000;
 
-// middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// rotas
+// ROTAS
+const authRoutes = require("./routes/authRoutes");
+
 app.use("/api/auth", authRoutes);
-app.use("/api/payment", paymentRoutes);
 
-// rota teste
+// rota raiz
 app.get("/", (req, res) => {
   res.send("Macave Mining API está online 🚀");
 });
 
-// MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB conectado!");
-    app.listen(PORT, () =>
-      console.log(`Servidor rodando na porta ${PORT}`)
-    );
-  })
-  .catch((err) => {
-    console.error("Erro ao conectar MongoDB:", err);
-  });
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
+});
