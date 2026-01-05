e452e1e1d9b18635411cbdccea45ae5b7fb4999c
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-05 14:26:52 +0200

    Update authRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware"); // caminho correto
 
-// Rota de teste para usuário autenticado
-router.get("/me", authMiddleware, (req, res) => {
-  res.json({
-    id: req.userId,
-    name: "Usuário Macave",
-    email: "teste@macave.com",
-    balance: 0
-  });
+// rota de teste
+router.get("/", (req, res) => {
+  res.json({ message: "Auth API OK" });
 });
-
-// Aqui você pode adicionar login, register e pagamento com criptomoeda
 
 module.exports = router;
