90d822cdaa959eed4a5a231b846bbffe1726e03f
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 06:48:57 +0200

    Update User.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,9 @@
-exports.payWithCrypto = (req, res) => {
-  const { userId, amount, currency } = req.body;
-  // Aqui você conecta à API de cripto real
-  res.json({ message: `Pagamento de ${amount} ${currency} recebido para ${userId}` });
-};
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  saldo: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model("User", userSchema);
