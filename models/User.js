5a04f414b05f3eee402a3dc9a7306208a94dbbaa
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 07:04:12 +0200

    Update User.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- models/User.js
+++ models/User.js
@@ -1,16 +1,10 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  balance: {
-    type: Number,
-    default: 0
-  },
-  role: {
-    type: String,
-    default: "user" // user | admin
-  }
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  balance: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("User", UserSchema);
