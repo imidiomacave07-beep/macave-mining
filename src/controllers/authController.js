7727b503cb0648a7e3e76196d661f4ee1c95adcd
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 06:45:08 +0200

    Update authController.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,24 +1,28 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.registerUser = async (req, res) => {
+exports.register = async (req, res) => {
   try {
-    const user = await User.create(req.body);
-    res.json({ user });
+    const { username, password } = req.body;
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashed });
+    res.status(201).json({ message: "Usuário criado", user });
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(500).json({ error: err.message });
   }
 };
 
-exports.loginUser = async (req, res) => {
+exports.login = async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user || user.password !== req.body.password) {
-      return res.status(401).json({ error: "Credenciais inválidas" });
-    }
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: "Usuário não encontrado" });
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) return res.status(401).json({ error: "Senha incorreta" });
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    res.json({ token });
+    res.json({ message: "Login feito", token });
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(500).json({ error: err.message });
   }
 };
