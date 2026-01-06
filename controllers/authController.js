d0c7b787b229d6a6369d0e4a63af3da81682341f
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 07:05:39 +0200

    Update authController.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,34 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-
+exports.register = async (req, res) => {
   try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ message: "Usuário não encontrado" });
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Preencha todos os campos" });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ message: "Senha incorreta" });
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "Usuário já existe" });
     }
 
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1d" }
-    );
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    res.json({ token });
-  } catch (err) {
-    res.status(500).json({ message: "Erro no servidor" });
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: "Usuário registrado com sucesso" });
+
+  } catch (error) {
+    console.error("ERRO REGISTO:", error);
+    res.status(500).json({ message: "Erro ao registrar" });
   }
 };
