1679e61392d0a611d1df8fe3a7732de36f20bd85
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 07:24:48 +0200

    Create plans.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ src/data/plans.js
@@ -0,0 +1,25 @@
+const plans = [
+  {
+    id: 1,
+    name: "Plano Básico",
+    price: 10,
+    dailyProfit: 0.5,
+    duration: 30
+  },
+  {
+    id: 2,
+    name: "Plano Pro",
+    price: 50,
+    dailyProfit: 3,
+    duration: 30
+  },
+  {
+    id: 3,
+    name: "Plano VIP",
+    price: 100,
+    dailyProfit: 7,
+    duration: 30
+  }
+];
+
+module.exports = plans;
