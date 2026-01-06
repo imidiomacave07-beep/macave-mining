c00f2cc81a83c0df723a965b91a3f40864fd047f
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 08:31:38 +0200

    Update dashboardController.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,6 +1,11 @@
-exports.getDashboard = (req, res) => {
-  res.json({
-    saldo: 0,
-    planosAtivos: []
-  });
+const User = require("../models/User");
+
+exports.getDashboard = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) return res.status(404).json({ msg: "Usuário não encontrado" });
+    res.json({ username: user.username, balance: user.balance });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
