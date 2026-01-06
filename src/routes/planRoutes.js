e97414a61b9bc8b1fcce06f3dadb8981eaab9a8a
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 07:26:02 +0200

    Update planRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/routes/planRoutes.js
+++ src/routes/planRoutes.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const plans = require("../data/plans");
 
-// rota de teste para planos
 router.get("/", (req, res) => {
-  res.json({
-    plans: [
-      { id: 1, name: "Plano Bronze", profit: 5 },
-      { id: 2, name: "Plano Prata", profit: 10 },
-      { id: 3, name: "Plano Ouro", profit: 20 }
-    ]
-  });
-});
-
-// rota para detalhes de um plano
-router.get("/:id", (req, res) => {
-  const planId = req.params.id;
-  res.json({ id: planId, name: `Plano ${planId}`, profit: planId * 5 });
+  res.json(plans);
 });
 
 module.exports = router;
