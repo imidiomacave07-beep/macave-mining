bf03b57027e5cb73fb7d119d22c826e07bdd86e0
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-06 09:13:36 +0200

    Update dashboardRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,20 +1,9 @@
+// routes/dashboardRoutes.js
 const express = require("express");
 const router = express.Router();
 const { getDashboard } = require("../controllers/dashboardController");
-const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (!token) return res.status(401).json({ msg: "Sem token" });
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: "Token inválido" });
-  }
-};
-
-router.get("/", authMiddleware, getDashboard);
+// Rota GET para dashboard
+router.get("/", getDashboard);
 
 module.exports = router;
