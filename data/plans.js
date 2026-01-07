c82c7a85ae0220535b1a60fbaf629d5c75ce7705
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-08 00:43:33 +0200

    Create plans.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ data/plans.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: "bronze",
+    name: "Plano Bronze",
+    price: 10,
+    dailyProfit: 2,
+    duration: 7
+  },
+  {
+    id: "prata",
+    name: "Plano Prata",
+    price: 25,
+    dailyProfit: 3,
+    duration: 15
+  },
+  {
+    id: "ouro",
+    name: "Plano Ouro",
+    price: 50,
+    dailyProfit: 5,
+    duration: 30
+  }
+];
