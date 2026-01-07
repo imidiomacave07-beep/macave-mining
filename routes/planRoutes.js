6c06e1b722166249a017c0f5eb79c32d1b1d1976
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-07 18:17:12 +0200

    Update planRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const Plan = require("../models/Plan");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const plans = await Plan.find();
-  res.json(plans);
+// Planos hardcoded
+const planos = [
+  { _id: "bronze", name: "Plano Bronze", price: 10, dailyProfit: 2, duration: 7 },
+  { _id: "prata", name: "Plano Prata", price: 25, dailyProfit: 3, duration: 15 },
+  { _id: "ouro", name: "Plano Ouro", price: 50, dailyProfit: 5, duration: 30 }
+];
+
+// Retorna os planos
+router.get("/", (req, res) => {
+  res.json(planos);
 });
 
 module.exports = router;
