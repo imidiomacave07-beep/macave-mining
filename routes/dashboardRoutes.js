92f6589143b36bb66bf819112a6023a657079553
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-07 15:54:37 +0200

    Update dashboardRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,8 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-const { getDashboard } = require("../controllers/dashboardController");
-
-router.get("/", getDashboard);
+router.get('/:userId', async (req, res) => {
+  const user = await User.findById(req.params.userId).populate('purchasedPlans');
+  if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
+  res.json({
+    balance: user.balance,
+    purchasedPlans: user.purchasedPlans
+  });
+});
 
 module.exports = router;
