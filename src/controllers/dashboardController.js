7a38ff79fd5b242ad41fe0e6e64d1b932c360d41
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-07 07:10:38 +0200

    Update dashboardController.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,9 +1,7 @@
-// controllers/dashboardController.js
-
 exports.getDashboard = (req, res) => {
-  // exemplo de resposta simples
   res.json({
-    balance: 0,
-    plans: []
+    saldo: 0,
+    planosAtivos: [],
+    status: "ok"
   });
 };
