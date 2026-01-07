9dcade1bfd32bd38cf7bad7bbe5ef3cedc05cb06
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-07 07:43:23 +0200

    Update dashboardRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,9 +1,8 @@
-// routes/dashboardRoutes.js
 const express = require("express");
 const router = express.Router();
+
 const { getDashboard } = require("../controllers/dashboardController");
 
-// rota GET para dashboard
 router.get("/", getDashboard);
 
 module.exports = router;
