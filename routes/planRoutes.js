5ada5fff6f42942c9bb9af8ea29d6e9550b55cfb
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-08 07:02:45 +0200

    Update planRoutes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -1,16 +1,35 @@
 const express = require("express");
+const Plan = require("../models/Plan");
+const Purchase = require("../models/Purchase");
+
 const router = express.Router();
 
-// Planos hardcoded
-const planos = [
-  { _id: "bronze", name: "Plano Bronze", price: 10, dailyProfit: 2, duration: 7 },
-  { _id: "prata", name: "Plano Prata", price: 25, dailyProfit: 3, duration: 15 },
-  { _id: "ouro", name: "Plano Ouro", price: 50, dailyProfit: 5, duration: 30 }
-];
+/* CRIAR PLANOS (executa 1 vez) */
+router.get("/seed", async (req, res) => {
+  await Plan.deleteMany();
 
-// Retorna os planos
-router.get("/", (req, res) => {
-  res.json(planos);
+  await Plan.insertMany([
+    { name: "Plano Bronze", price: 10, dailyProfit: 2, duration: 7 },
+    { name: "Plano Prata", price: 25, dailyProfit: 3, duration: 15 },
+    { name: "Plano Ouro", price: 50, dailyProfit: 5, duration: 30 }
+  ]);
+
+  res.json({ message: "Planos criados" });
+});
+
+/* LISTAR PLANOS */
+router.get("/", async (req, res) => {
+  const plans = await Plan.find();
+  res.json(plans);
+});
+
+/* COMPRAR PLANO */
+router.post("/buy", async (req, res) => {
+  const { userId, planId } = req.body;
+
+  await Purchase.create({ userId, planId });
+
+  res.json({ message: "Plano comprado com sucesso" });
 });
 
 module.exports = router;
