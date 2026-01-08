bc972463f90bcfffbafcc6e948a8eecc150a1176
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-08 07:10:05 +0200

    Update User.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- models/User.js
+++ models/User.js
@@ -1,28 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+const mongoose = require("mongoose");
 
-const authRoutes = require('./routes/authRoutes');
-const dashboardRoutes = require('./routes/dashboardRoutes');
-const planRoutes = require('./routes/planRoutes');
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  balance: { type: Number, default: 0 }
+});
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Rotas
-app.use('/api/auth', authRoutes);
-app.use('/api/dashboard', dashboardRoutes);
-app.use('/api', planRoutes);
-
-const PORT = process.env.PORT || 10000;
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB conectado!');
-    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-  })
-  .catch(err => console.error('Erro MongoDB:', err));
+module.exports = mongoose.model("User", UserSchema);
