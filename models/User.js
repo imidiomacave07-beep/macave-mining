93a427f01e7fbe531aa1572c2e1ef4e56363dc2a
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-08 18:07:27 +0200

    Update User.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- models/User.js
+++ models/User.js
@@ -1,28 +1,19 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
 
-const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  balance: { type: Number, default: 100 },
-  isAdmin: { type: Boolean, default: false },
-  wallet: { type: String, default: "" },
-  plans: [
-    {
-      id: Number,
-      name: String,
-      profit: Number,
-      wallet: String
-    }
-  ],
-  withdraws: [
-    {
-      amount: Number,
-      method: String,
-      destination: String,
-      date: String,
-      status: String
-    }
-  ]
+const withdrawSchema = new mongoose.Schema({
+  amount: Number,
+  method: String,
+  destination: String,
+  date: String,
+  status: { type: String, default: "pendente" }
 });
 
-module.exports = model("User", userSchema);
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  balance: { type: Number, default: 0 },
+  isAdmin: { type: Boolean, default: false },
+  withdraws: [withdrawSchema]
+});
+
+module.exports = mongoose.model("User", userSchema);
