02270ac2a0ce4b641197cbf9375bc667a4c5e9d4
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-08 09:22:48 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
-const Withdrawal = require("./models/Withdrawal");
-
-app.post("/api/withdraw", async (req, res) => {
-  try {
-    const { userId, amount, mpesaNumber } = req.body;
-
-    if (!amount || amount <= 0) {
-      return res.json({ message: "Valor inválido" });
-    }
-
-    const user = await User.findById(userId);
-
-    if (!user || user.balance < amount) {
-      return res.json({ message: "Saldo insuficiente" });
-    }
-
-    user.balance -= amount;
-    await user.save();
-
-    const withdrawal = new Withdrawal({
-      userId,
-      amount,
-      mpesaNumber
-    });
-
-    await withdrawal.save();
-
-    res.json({ message: "Pedido de saque enviado com sucesso!" });
-  } catch (err) {
-    res.status(500).json({ message: "Erro no saque" });
+{
+  "name": "imidio-mining",
+  "version": "1.0.0",
+  "main": "backend/server.js",
+  "scripts": { "start": "node backend/server.js" },
+  "dependencies": {
+    "bcrypt": "^5.1.0",
+    "body-parser": "^1.20.2",
+    "express": "^4.18.2",
+    "jsonwebtoken": "^9.0.0"
   }
-});
+}
