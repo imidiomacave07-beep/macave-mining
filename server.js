bb7efb8c7a8b1616699e2e192174b9ec9193d584
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-09 17:11:28 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ const plans = require("./backend/plans");
 const purchasePlan = require("./backend/purchase");
 const requestWithdraw = require("./backend/withdraw");
 
+// Rotas
 app.get("/api/plans", (req, res) => res.json(plans));
 app.post("/api/plans/buy", purchasePlan);
-
-// Nova rota para saque
 app.post("/api/withdraw/request", requestWithdraw);
 
 app.listen(3000, () => console.log("🚀 Macave Mining API rodando"));
