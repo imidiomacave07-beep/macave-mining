4a27db65a51cb40cb1677755967bb7300b1e497b
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-09 16:30:20 +0200

    Create plans.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ backend/plans.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: "start",
+    name: "START",
+    price: 10,
+    dailyMin: 0.8,
+    dailyMax: 1.0,
+    duration: 30
+  },
+  {
+    id: "basic",
+    name: "BASIC",
+    price: 25,
+    dailyMin: 1.0,
+    dailyMax: 1.2,
+    duration: 30
+  },
+  {
+    id: "pro",
+    name: "PRO",
+    price: 50,
+    dailyMin: 1.2,
+    dailyMax: 1.5,
+    duration: 30
+  },
+  {
+    id: "advanced",
+    name: "ADVANCED",
+    price: 100,
+    dailyMin: 1.4,
+    dailyMax: 1.7,
+    duration: 30
+  },
+  {
+    id: "vip",
+    name: "VIP",
+    price: 200,
+    dailyMin: 1.6,
+    dailyMax: 2.0,
+    duration: 30
+  }
+];
