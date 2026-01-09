117f75ac431f44c7df71d13e31cb97c73a1ebf32
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-09 17:39:28 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -13,4 +13,22 @@ app.get("/api/plans", (req, res) => res.json(plans));
 app.post("/api/plans/buy", purchasePlan);
 app.post("/api/withdraw/request", requestWithdraw);
 
+// Função de lucro diário automática
+function calcularLucroDiario() {
+  const hoje = new Date().toDateString();
+  Object.values(global.users || {}).forEach(user => {
+    if (user.lastProfitDate === hoje) return;
+    user.activePlans.forEach(plan => {
+      const ganho = plan.price * (Math.random() * (plan.dailyMax - plan.dailyMin) + plan.dailyMin) / 100;
+      user.balance += ganho;
+    });
+    user.lastProfitDate = hoje;
+  });
+}
+
+// Rodar cálculo ao iniciar e a cada hora
+if (!global.users) global.users = {};
+calcularLucroDiario();
+setInterval(calcularLucroDiario, 60 * 60 * 1000);
+
 app.listen(3000, () => console.log("🚀 Macave Mining API rodando"));
