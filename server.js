850bd8d3b377fbb2749e0805f431146d667cf332
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-09 16:35:02 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,45 +1,19 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-const User = require("./models/User");
-const { getDailyMiningIncome } = require("./binancePool");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+const plans = require("./backend/plans");
+const purchasePlan = require("./backend/purchase");
 
-/* DISTRIBUIÇÃO REAL */
-app.post("/api/mining/distribute", async (req, res) => {
-  const income = await getDailyMiningIncome();
-
-  if (!income.length)
-    return res.json({ message: "Nenhum lucro hoje" });
-
-  const totalBTC = income.reduce(
-    (sum, i) => sum + Number(i.profitAmount),
-    0
-  );
-
-  const users = await User.find({ "plans.0": { $exists: true } });
-
-  let totalShare = 0;
-  users.forEach(u =>
-    u.plans.forEach(p => (totalShare += p.profitShare))
-  );
-
-  for (const user of users) {
-    let userShare = 0;
-    user.plans.forEach(p => (userShare += p.profitShare));
-
-    const userProfit = (userShare / totalShare) * totalBTC;
-    user.balance += userProfit;
-    await user.save();
-  }
-
-  res.json({ message: "Lucro distribuído", totalBTC });
+app.get("/api/plans", (req, res) => {
+  res.json(plans);
 });
 
-app.listen(3000, () =>
-  console.log("🚀 Macave Mining + Binance Pool conectado")
-);
+app.post("/api/plans/buy", purchasePlan);
+
+app.listen(3000, () => {
+  console.log("🚀 Macave Mining API rodando");
+});
