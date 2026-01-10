89eec180382feed52c4eba95e691cff7d015f10b
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-10 08:20:06 +0200

    Update plans.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- backend/plans.js
+++ backend/plans.js
@@ -1,31 +1,6 @@
-// backend/plans.js
 module.exports = [
-  {
-    id: 1,
-    name: "Plano Básico",
-    price: 20,
-    daily: 1.5,
-    days: 30
-  },
-  {
-    id: 2,
-    name: "Plano Standard",
-    price: 50,
-    daily: 2,
-    days: 30
-  },
-  {
-    id: 3,
-    name: "Plano Pro",
-    price: 100,
-    daily: 2.5,
-    days: 40
-  },
-  {
-    id: 4,
-    name: "Plano Premium",
-    price: 200,
-    daily: 3,
-    days: 45
-  }
+  { id: 1, name: "Plano Básico", price: 20, daily: 1.5, days: 30 },
+  { id: 2, name: "Plano Standard", price: 50, daily: 2, days: 30 },
+  { id: 3, name: "Plano Pro", price: 100, daily: 2.5, days: 40 },
+  { id: 4, name: "Plano Premium", price: 200, daily: 3, days: 45 }
 ];
