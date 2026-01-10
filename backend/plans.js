0f01d8026bc0727c9551e70dc876150abb1c917d
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-10 09:01:24 +0200

    Add new 'plano macave' to plans list
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- backend/plans.js
+++ backend/plans.js
@@ -1,6 +1,8 @@
+// backend/plans.js
 module.exports = [
   { id: 1, name: "Plano Básico", price: 20, daily: 1.5, days: 30 },
   { id: 2, name: "Plano Standard", price: 50, daily: 2, days: 30 },
   { id: 3, name: "Plano Pro", price: 100, daily: 2.5, days: 40 },
   { id: 4, name: "Plano Premium", price: 200, daily: 3, days: 45 }
 ];
+{ id: 5, name: "plano macave", price: 300, daily: 6, days: 60}
