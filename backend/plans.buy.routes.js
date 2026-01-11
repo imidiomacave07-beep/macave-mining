b44e32b038b351f42db84d371eec85465d6ba552
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-11 19:45:43 +0200

    Update plans.buy.routes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- backend/plans.buy.routes.js
+++ backend/plans.buy.routes.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
-const plans = [
-  { id: 1, name: "Starter", price: 20, dailyMin: 1.2, dailyMax: 1.8 },
-  { id: 2, name: "Basic", price: 50, dailyMin: 1.5, dailyMax: 2.2 },
-  { id: 3, name: "Pro", price: 100, dailyMin: 2.0, dailyMax: 2.8 },
-  { id: 4, name: "Advanced", price: 200, dailyMin: 2.5, dailyMax: 3.5 }
+let plans = [
+  {
+    id: 1,
+    name: "Starter",
+    price: 20,
+    dailyMin: 1.2,
+    dailyMax: 1.8,
+    duration: 30
+  },
+  {
+    id: 2,
+    name: "Basic",
+    price: 50,
+    dailyMin: 1.5,
+    dailyMax: 2.2,
+    duration: 30
+  },
+  {
+    id: 3,
+    name: "Pro",
+    price: 100,
+    dailyMin: 2.0,
+    dailyMax: 2.8,
+    duration: 30
+  },
+  {
+    id: 4,
+    name: "Advanced",
+    price: 200,
+    dailyMin: 2.5,
+    dailyMax: 3.5,
+    duration: 30
+  }
 ];
 
+// listar planos
 router.get("/", (req, res) => {
   res.json(plans);
 });
