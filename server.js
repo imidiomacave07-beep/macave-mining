1ca0c9d2f91559d2733791e7649be8c8a78e949b
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-01-11 19:33:25 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -7,17 +7,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// ROTAS
-const plansRoutes = require("./backend/plans.routes");
+// Rotas
 const authRoutes = require("./backend/auth.routes");
+const plansRoutes = require("./backend/plans.routes");
+const withdrawRoutes = require("./backend/withdraw.routes");
 
-app.use("/api/plans", plansRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/plans", plansRoutes);
+app.use("/api/withdraw", withdrawRoutes);
 
-app.get("/", (req, res) => {
-  res.send("🚀 Macave Mining API rodando");
-});
-
-app.listen(PORT, () => {
-  console.log("🚀 Macave Mining API rodando na porta " + PORT);
-});
+app.listen(PORT, () => console.log(`🚀 Macave Mining API rodando na porta ${PORT}`));
