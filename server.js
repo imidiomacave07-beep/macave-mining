32b7cd34db7dc15772fbe57a66a5b54ef251fa0c
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-03-28 23:22:23 +0200

    Clean up server.js by removing comments
    Removed commented sections for routes and pages.
    
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -9,12 +9,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Rotas
 app.use('/api/auth', require('./backend/auth.routes'));
 app.use('/api/plans', require('./backend/plans.routes'));
 app.use('/api/wallet', require('./backend/wallet.routes'));
 
-// Páginas
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
