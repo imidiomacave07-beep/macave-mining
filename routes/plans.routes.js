ba726a5215f5b930fe128f52c784c6f5d892e0d2
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-03-30 22:54:50 +0200

    Update plans.routes.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/plans.routes.js
+++ routes/plans.routes.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const plansController = require('../controllers/plans.controller');
+const express = require("express")
+const router = express.Router()
+const User = require("../models/User")
+const auth = require("../middleware/auth")
 
-router.get('/', plansController.getPlans);
-router.post('/buy', plansController.buyPlan);
-router.get('/balance/:userId', plansController.getUserBalance);
+const REF_COMMISSION = 0.05
 
-module.exports = router;
+router.post("/buy", auth, async (req,res)=>{
+  const { planName, amount } = req.body
+  const user = await User.findById(req.userId)
+  if(user.balance < amount) return res.status(400).json({message:"Saldo insuficiente"})
+
+  user.balance -= amount
+  user.invested += amount
+  await user.save()
+
+  if(user.referral){
+    const refUser = await User.findOne({ referralCode:user.referral })
+    if(refUser){
+      const commission = amount * REF_COMMISSION
+      refUser.balance += commission
+      refUser.referralCommission += commission
+      await refUser.save()
+    }
+  }
+
+  res.json({ balance:user.balance })
+})
+
+module.exports = router
