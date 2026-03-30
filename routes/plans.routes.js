f7383e0b53da31f241d8ac12728347053c1ab994
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-03-30 13:07:38 +0200

    Refactor plans routes to use controller methods
    Removed hardcoded plans data and updated route handlers.
    
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/plans.routes.js
+++ routes/plans.routes.js
@@ -1,18 +1,9 @@
-const express = require("express")
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const plansController = require('../controllers/plans.controller');
 
-// Planos de $10 a $5000
-const planos = [
-  { id: 1, name: "Starter", roi: 1, days: 20, min: 10, max: 99 },
-  { id: 2, name: "Basic", roi: 1.5, days: 30, min: 100, max: 499 },
-  { id: 3, name: "Standard", roi: 2, days: 30, min: 500, max: 999 },
-  { id: 4, name: "Advanced", roi: 2.5, days: 45, min: 1000, max: 2999 },
-  { id: 5, name: "VIP", roi: 3, days: 60, min: 3000, max: 5000 }
-]
+router.get('/', plansController.getPlans);
+router.post('/buy', plansController.buyPlan);
+router.get('/balance/:userId', plansController.getUserBalance);
 
-router.get("/", (req, res) => {
-  res.json(planos)
-})
-
-module.exports = router
-module.exports.planos = planos
+module.exports = router;
