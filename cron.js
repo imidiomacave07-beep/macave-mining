921d499291bc43192bb1ca675ce06380079e095f
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-03-31 14:07:21 +0200

    Update cron.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- cron.js
+++ cron.js
@@ -1,6 +1,6 @@
-const { distributeProfits } = require('./controllers/cloud.controller');
+const miningService = require("./services/mining.service");
 
 setInterval(async () => {
-  console.log('Distribuindo lucros...');
-  await distributeProfits();
-}, 60 * 60 * 1000); // a cada 1 hora
+  const balance = await miningService.getBalance();
+  console.log("Lucro diário atualizado:", balance);
+}, 24 * 60 * 60 * 1000); // 24 horas
