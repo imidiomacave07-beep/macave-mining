cb8970a4c93df8e0a5cffc47444d6c25fb043c4b
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-03-31 14:05:31 +0200

    Create plans.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ plans.js
@@ -0,0 +1,10 @@
+const plans = [
+  { name: "Starter", price: 10, hashrate: 1, duration: 30 },
+  { name: "Basic", price: 50, hashrate: 5, duration: 30 },
+  { name: "Standard", price: 100, hashrate: 10, duration: 30 },
+  { name: "Pro", price: 500, hashrate: 50, duration: 30 },
+  { name: "Enterprise", price: 1000, hashrate: 100, duration: 30 },
+  { name: "Institutional", price: 5000, hashrate: 500, duration: 30 },
+];
+
+module.exports = plans;
