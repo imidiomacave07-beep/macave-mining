b1bad1568feb87a43e09c9ed1720e322db564243
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-04-16 20:06:24 +0200

    Update Withdrawal.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- models/Withdrawal.js
+++ models/Withdrawal.js
@@ -1,11 +1,31 @@
 const mongoose = require("mongoose");
 
 const WithdrawalSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  amount: Number,
-  mpesaNumber: String,
-  status: { type: String, default: "Pendente" },
-  createdAt: { type: Date, default: Date.now }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  amount: {
+    type: Number,
+    required: true
+  },
+
+  wallet: {
+    type: String,
+    default: ""
+  },
+
+  status: {
+    type: String,
+    default: "pending" // pending, approved, rejected
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("Withdrawal", WithdrawalSchema);
