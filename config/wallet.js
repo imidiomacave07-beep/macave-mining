90d29105e9bd60bced94f519662f91714b7f2ed8
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-04-18 00:26:16 +0200

    Create wallet.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ config/wallet.js
@@ -0,0 +1,6 @@
+module.exports = {
+  trc20Wallet: process.env.USDT_TRC20_WALLET,
+  erc20Wallet: process.env.USDT_ERC20_WALLET,
+  networkTRC20: process.env.NETWORK_TRC20,
+  networkERC20: process.env.NETWORK_ERC20,
+};
