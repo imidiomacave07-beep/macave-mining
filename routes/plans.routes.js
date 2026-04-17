2642e92620c19c867841f84e8ef50963feaa73cc
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-04-17 07:46:54 +0200

    Refactor plans.routes.js to remove buy route
    Removed the buy plan route and added a test route.
    
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- routes/plans.routes.js
+++ routes/plans.routes.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const miningService = require("../services/mining.service");
-const plans = require("../plans");
 
-router.post("/buy/:planName", async (req, res) => {
-  const plan = plans.find(p => p.name === req.params.planName);
-  if (!plan) return res.status(404).send("Plano não encontrado");
-
-  await miningService.startMining(plan.hashrate);
-
-  res.send({ message: `Plano ${plan.name} comprado com ${plan.hashrate} TH/s` });
+/* EXEMPLO DE ROTA */
+router.get("/", (req, res) => {
+  res.json({ message: "Plans route working" });
 });
 
 module.exports = router;
