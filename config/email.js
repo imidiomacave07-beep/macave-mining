6071169521d8b8ff4fe7f830ffc67ebba89bccc7
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-04-19 15:36:02 +0200

    Add email configuration module
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ config/email.js
@@ -0,0 +1,9 @@
+module.exports = {
+  supportEmail: process.env.SUPPORT_EMAIL,
+  financeEmail: process.env.FINANCE_EMAIL,
+  adminEmail: process.env.ADMIN_EMAIL,
+  smtpHost: process.env.SMTP_HOST,
+  smtpPort: process.env.SMTP_PORT,
+  smtpUser: process.env.SMTP_USER,
+  smtpPass: process.env.SMTP_PASS,
+};
