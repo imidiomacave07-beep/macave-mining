6ce0a92eb66ff9d1fe61546a5237df4e4b1ba5d4
imidiomacave07-beep <imidiomacave07@gmail.com>
2026-04-19 15:43:52 +0200

    Update index.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- index.js
+++ index.js
@@ -4,21 +4,37 @@ require("dotenv").config();
 
 const app = express();
 
+/* =========================
+   MIDDLEWARE
+========================= */
 app.use(cors());
 app.use(express.json());
 
 /* =========================
-   HEALTH CHECK
+   HEALTH CHECK (PING)
 ========================= */
 app.get("/ping", (req, res) => {
   res.status(200).send("ok");
 });
 
 /* =========================
-   ROUTES
+   HOME ROUTE
 ========================= */
+app.get("/", (req, res) => {
+  res.send("Macave Mining API is running...");
+});
+
+/* =========================
+   ROUTES IMPORT
+========================= */
+
+// Deposit Routes
 const depositRoutes = require("./routes/deposit.routes");
 app.use("/deposit", depositRoutes);
+
+// Contact / Support Routes
+const contactRoutes = require("./routes/contact.routes");
+app.use("/contact", contactRoutes);
 
 /* =========================
    SERVER START
